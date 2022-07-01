ca390fbb6ef47fa77d85b5ca71fa304f9b3f3fe2
keithbarnatchez <97457120+keithbarnatchez@users.noreply.github.com>
2022-07-01 11:18:40 -0400

    Updates to plotting function: increased customizability and some custom error messages to handle scenarios where user supplies default values that weren't simulated

--- R/error_confounder/testing.R
+++ R/error_confounder/testing.R
@@ -16,12 +16,8 @@ source('output_functions.R')
 n <- 10000 # obs 
 
 # Generate the data
-for (i in 1:40) {
-  print(i)
 data <- generate_data(n,sig_u = 0.3,binary=0,
                       ba=1) 
-psc(data)
-}
 #-------------------------------------------------
 # Test correction functions 
 
@@ -34,10 +30,10 @@ ate_simex <- simex_indirect(data)
 # ------------------------------------------------
 # Test the get_results() function
 
-methods <- c('iv')
-sig_u_grid <- c(0.1,0.2,0.3,0.5,0.9) ; bt_grid <- c(1) ; n_grid <- c(5000)
+methods <- c('iv') 
+sig_u_grid <- c(0.1,0.2,0.3,0.5,0.9) ; bt_grid <- c(0.5,1,1.5) ; n_grid <- c(1000,3000,5000)
 bin_grid <- c(0)
-op_chars2 <- get_results(methods,sig_u_grid,bt_grid,n_grid, bin_grid, nsim=100)
+op_chars2 <- get_results(ethods,sig_u_grid,bt_grid,n_grid, bin_grid, nsim=100)
 
 # ------------------------------
 # Plot 
--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -1,15 +1,46 @@
+set_plot_theme <- function() {
+  # theme_set(theme_bw() +
+  #             theme(plot.title = element_text(hjust = 0, size = 16),
+  #                   plot.subtitle = element_text(hjust = 0, size = 12),
+  #                   axis.title = element_text(size = 12),
+  #                   strip.text = element_text(size = 12),
+  #                   legend.position = "bottom"))
+  ggplot2::theme_set(
+    ggplot2::theme_minimal() +
+      ggplot2::theme(
+        panel.grid.minor = ggplot2::element_blank(),
+        plot.title = ggplot2::element_text(
+          face = "bold"
+        ),
+        plot.subtitle = ggplot2::element_text(
+          margin = ggplot2::margin(b = 10)
+        ),
+        strip.text = ggplot2::element_text(
+          face = "bold",
+          hjust = 0,
+          margin = ggplot2::margin(b = 5),
+          size = 11
+        )
+      )
+  )
+}
+
 line_plot <- function(op_chars,xvar,yvar,
-                      xlab='g',
-                      ylab='g',
+                      xlab,
+                      ylab,
+                      plt_title,
+                      fixed_vars=c('u','a','n','b'),
+                      fixed_defaults=c(0.3,1,5000,0),
+                      var_labs=c('ME variance:', 'True ATE:', 'n:', 'Binary outcome:'),
                       save=FALSE) {
   #' Function for generating plots of operating characteristics
   #'
   #' INPUTS:
   #' - op_chars
-  #' - xvar (u,a,n,b)
-  #' - yvar (bias,mse,ATE,ci_cov,power)
-  #' - xlab
-  #' - ylab
+  #' - xvar (u,a,n,b): A string containing the x variable for the plot
+  #' - yvar (bias,mse,ATE,ci_cov,power): A string for y variable
+  #' - xlab: x axis title
+  #' - ylab: y axis title 
   #' - save
   #' - filename
   #'
@@ -18,12 +49,49 @@ line_plot <- function(op_chars,xvar,yvar,
   #' and will save a file with name format xvar_yvar_data.pdf if save is set to TRUE
   #' 
   
-  # 
+  set_plot_theme()
+  
+  # Prune out the var that's being varied
+  drop_idx <- (fixed_vars %in% xvar)   
+  fixed_vars <- fixed_vars[! drop_idx] ; fixed_defaults <- fixed_defaults[! drop_idx]
+  var_labs <- var_labs[! drop_idx]
+  
+  
+  # Fix values at defaults via filtering
+  
+  for (i in 1:length(fixed_vars)) {
+  
+    # Assert that the default values are actually used  in the simulation.
+    # If not, throw an error message so the user can fix it
+    # <...> !( fixed_defaults[i] %in% op_chars[fixed_vars[i]] )
+    if ( !(  any(fixed_defaults[i] == op_chars[fixed_vars[i]])   )  ) {
+
+      stop(paste('Error: fixed val selected for',fixed_vars[i],'is not in operating characteristics dataframe. select a default that is in the dataframe'  ))
+    }
+    # Filter to only keep rows with default value of current var
+    op_chars <- op_chars %>% filter( eval(as.name(fixed_vars[i])) == fixed_defaults[i] )
+    
+    # Construct the figure subtitle
+    comma<-','
+    if (i==length(fixed_vars)) {comma<-''}
+    if (i==1) {
+      subt <- paste(var_labs[i], fixed_defaults[i],comma)
+    }
+    else{
+      subt <- paste(subt,var_labs[i], fixed_defaults[i],comma)
+    }
+  }
+  
+  # Make the plot
   op_chars %>% ggplot(aes(x=eval(as.name(xvar)),
                           y=eval(as.name(yvar)),
                           color=method)) + 
-               geom_line() + theme_bw() +
-               xlab(xlab) + ylab(ylab)
+               geom_line() + 
+               labs(x=xlab,
+                    y=ylab,
+                    title=plt_title,
+                    subtitle=subt)
+            
 }
 
-line_plot(op_chars,'u')
+line_plot(op_chars,'u','mse',xlab='Error variance',ylab='MSE',plt_title = 'MSE of ME correction methods, varying error variance')
