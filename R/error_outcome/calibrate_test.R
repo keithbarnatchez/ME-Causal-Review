89160e892c68e4c7b6848073823e28cffc658382
kevjosey <kjosey@hsph.harvard.edu>
2022-11-10 15:39:12 -0500

    Eureka! On outcome measurement error at least.

--- R/error_outcome/calibrate_test.R
+++ R/error_outcome/calibrate_test.R
@@ -13,7 +13,7 @@ n.sim <- 100
 
 # model arguments
 a.vals <- seq(6, 14, by = 0.02)
-n <- 2000
+n <- 4000
 sigma <- 2
 omega <- 1
 tau <- 0.5
@@ -35,13 +35,12 @@ out <- mclapply(1:n.sim, function(i, ...) {
   mu_out <- 2 - 0.75*x[,1] - 0.25*x[,2] + 0.25*x[,3] + 0.75*x[,4] +
     0.25*(a - 10) - 0.75*cos(pi*(a - 6)/4) - 0.25*(a - 10)*x[,1]
   
-  eta_out <- 0.25*x[,1] - 0.75*x[,2] + 0.75*x[,3] - 0.25*x[,4] +
-    0.25*(a - 10) - 0.25*x[,1]*(a - 10)^2
+  eta_out <- 0.5 + 0.25*x[,1] - 0.75*x[,2] + 0.75*x[,3] - 0.25*x[,4] + 0.25*(a - 6)
   
   y_true <- rnorm(n, mu_out, omega)
   y_me <- y_true + rnorm(n, eta_out, tau)
   
-  rho <- plogis(-1.5 + 0.5*x[,1] + 0.5*x[,2] - 0.5*x[,3] - 0.5*x[,4])
+  rho <- plogis(-1 + 0.5*x[,1] - 0.5*x[,4])
   val <- rbinom(n, 1, rho)
   
   true_erc <- sapply(a.vals, function(a.tmp, ...) {
@@ -60,8 +59,8 @@ out <- mclapply(1:n.sim, function(i, ...) {
   Y1_true <- y_true[val == 1]
   
   adjust <- out_bias(A0 = A0, A1 = A1, X0 = X0, X1 = X1, Y0 = Y0, Y1_me = Y1_me, 
-                     Y1_true = Y1_true, a.vals = a.vals, bw = 0.7)
-  naive <- out_naive(A0, X0, Y0, a.vals = a.vals, bw = 0.7)
+                     Y1_true = Y1_true, a.vals = a.vals, bw = 0.5)
+  naive <- out_naive(A0, X0, Y0, a.vals = a.vals, bw = 0.5)
   
   return(list(est = t(data.frame(true_erc = true_erc, naive_est = naive$estimate, adjust_est = adjust$estimate)),
               se = t(data.frame(adjust_se = adjust$se, naive_se = naive$se))))
@@ -83,24 +82,25 @@ rownames(out_est) <- c("True ERF","Naive","Bias Corrected")
 # Mean Absolute Error
 out_bias <- t(apply(est[2:3,,], 1, function(x) rowMeans(abs(x - mu.mat), na.rm = T)))
 colnames(out_bias) <- a.vals
-rownames(out_bias) <- c("Naive","Bias Corrected")
+rownames(out_bias) <- c("Naive","Corrected")
 
 # Mean Squared Error
 out_mse <- t(apply(est[2:3,,], 1, function(x) rowMeans((x - mu.mat)^2, na.rm = T)))
 colnames(out_mse) <- a.vals
-rownames(out_mse) <- c("Naive","Bias Corrected")
+rownames(out_mse) <- c("Naive","Corrected")
 
 # Coverage Probability
 cp <- list(as.matrix((est[2,,] - 1.96*se[1,,]) < mu.mat & (est[2,,] + 1.96*se[1,,]) > mu.mat),
            as.matrix((est[3,,] - 1.96*se[2,,]) < mu.mat & (est[3,,] + 1.96*se[2,,]) > mu.mat))
 out_cp <- do.call(rbind, lapply(cp, rowMeans, na.rm = T))
 colnames(out_cp) <- a.vals
-rownames(out_cp) <- c("Naive","Bias Corrected")
+rownames(out_cp) <- c("Naive","Corrected")
 
 # Plots
-plot(a.vals, out_est[1,], type = "l", col = "black", lwd = 2,
+plot(a.vals, out_est[1,], type = "l", col = "black", lwd = 2, ylim  = c(0,5),
      main = "Exposure Response Curve", xlab = "Exposure", ylab = "Rate of Event")
 lines(a.vals, out_est[2,], type = "l", col = "blue", lwd = 2, lty = 1)
 lines(a.vals, out_est[3,], type = "l", col = "red", lwd = 2, lty = 1)
-legend("topleft", legend=c("True ERF","Naive","Bias Corrected"),
+grid()
+legend("topleft", legend=c("True ERF","Naive ERF","Corrected ERF"),
        col= c("black", "blue", "red"), lty = c(1,1,1), lwd=2, cex=0.8)
