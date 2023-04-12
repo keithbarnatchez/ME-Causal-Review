6f30dba6a7e803100c87eed74083dd5bec1025d3
kevjosey <kjosey@hsph.harvard.edu>
2023-04-12 10:50:52 -0400

    Started sandwich variance estimation.

--- R/error_outcome/me_erf.R
+++ R/error_outcome/me_erf.R
@@ -1,6 +1,6 @@
 out_me <- function(A0, A1, X0, X1, Y0, Y1_me, Y1_true, 
                    a.vals = seq(min(A0), max(A0), length = 100), bw = 1, 
-                   family = gaussian(), sl.lib = c("SL.mean", "SL.glm"), ...) {
+                   family = gaussian(), se.fit = TRUE, ...) {
   
   n1 <- nrow(X1)
   n0 <- nrow(X0)
@@ -11,36 +11,6 @@ out_me <- function(A0, A1, X0, X1, Y0, Y1_me, Y1_true,
   
   n <- n1 + n0
   m <- ncol(X)
-  
-  ## outcome model
-  # mumod <- SuperLearner(Y = Y0, X = data.frame(X0[,-1], A = A0),
-  #                       family = family, SL.library = sl.lib)
-  # muhat <- mumod$SL.predict
-  # 
-  # muhat.mat <- sapply(a.vals, function(a.tmp, ...) {
-  # 
-  #   xa.tmp <- data.frame(X0, A = a.tmp)
-  #   predict(mumod, newdata = xa.tmp)$pred
-  # 
-  # })
-  # 
-  # mhat <- predict(smooth.spline(a.vals, colMeans(muhat.mat)), x = a)$y
-  # int0.mat <- matrix(rep(colMeans(muhat.mat), n0), byrow = TRUE, nrow = n0)
-  # 
-  # ## bias model
-  # bias <- c(Y1_me - Y1_true)
-  # etamod <- SuperLearner(Y = bias, X = data.frame(X1[,-1], A = A1),
-  #                        family = family, SL.library = sl.lib)
-  # 
-  # etahat.mat <- sapply(a.vals, function(a.tmp, ...) {
-  # 
-  #   xa.tmp <- data.frame(X0, A = a.tmp)
-  #   predict(mumod, newdata = xa.tmp)$pred
-  # 
-  # })
-  # 
-  # ehat <- predict(smooth.spline(a.vals, colMeans(etahat.mat)), x = a)$y
-  # int1.mat <- matrix(rep(colMeans(etahat.mat), n1), byrow = TRUE, nrow = n1)
   
   ## weight model
   
@@ -54,25 +24,25 @@ out_me <- function(A0, A1, X0, X1, Y0, Y1_me, Y1_true,
   astar2 <- c(astar2_0, astar2_1)
   
   cmat <- cbind((1 - S)*X*astar, (1 - S)*astar2,
-                S*X*astar, S*astar2, S*X, (1-S)*X)
-  target <- c(rep(0, 2*(m + 1)), n1*colMeans(X0), n0*colMeans(X0))
+                S*X*astar, S*astar2, (1-S)*X, S*X)
+  target <- c(rep(0, (2*m + 2)), n0*colMeans(X0), n1*colMeans(X0))
   
   mod <- calibrate(cmat = cmat, target = target)
   ipw <- mod$weights
   
-  psi0 <- c(ipw[S == 0]*Y0)
-  psi1 <- c(ipw[S == 1]*c(Y1_me - Y1_true))
+  Y <- c(Y0, Y1_me - Y1_true)
+  psi <- Y*ipw
   
   ## kernel weighted least squares
-  out0 <- sapply(a.vals, kern_ipw, a = A0, psi = psi0, bw = bw, se.fit = TRUE)
-  out1 <- sapply(a.vals, kern_ipw, a = A1, psi = psi1, bw = bw, se.fit = TRUE)
+  out <- sapply(a.vals, kern_ipw, a = A, s = S, x = X, psi = psi, ipw = ipw, 
+                cmat = cmat, astar = astar, astar2 = astar2, bw = bw, se.fit = se.fit)
   
   ## spline method
   # out0 <- spl_ipw(a = A0, psi = psi0, a.vals = a.vals, df = 6, se.fit = TRUE)
   # out1 <- spl_ipw(a = A1, psi = psi1, a.vals = a.vals, df = 6, se.fit = TRUE)
   
-  estimate <- out0[1,] - out1[1,]
-  se <- sqrt(out0[2,] + out1[2,])
+  estimate <- out[1,]
+  se <- sqrt(out[2,])
   
   return(list(estimate = estimate, se = se, ipw0 = ipw[S == 0], ipw1 = ipw[S == 1]))
   
@@ -97,7 +67,7 @@ out_naive <- function(A0, X0, Y0, a.vals = seq(min(A0), max(A0), length = 100), 
   
   ## kernel weighted least squares
   psi0 <- c(ipw*Y0)
-  out <- sapply(a.vals, kern_ipw, a = A0, psi = psi0, bw = bw, se.fit = TRUE)
+  out <- sapply(a.vals, kern_naive, a = A0, psi = psi0, bw = bw, se.fit = TRUE)
   
   estimate <- out[1,]
   se <- sqrt(out[2,])
@@ -106,8 +76,89 @@ out_naive <- function(A0, X0, Y0, a.vals = seq(min(A0), max(A0), length = 100), 
   
 }
 
+esteq <- function(p, s, x, psi, g.std, astar, astar2, ipw, eta, theta) {
+  
+  eq1 <- (1 - s)*p*x*astar
+  eq2 <- (1 - s)*p*astar2
+  eq3 <- s*p*x*astar
+  eq4 <- s*p*astar2
+  
+  eq5 <- (1 - s)*(p*x - theta)
+  eq6 <- s*(p*x - theta)
+  eq7 <- (1 - s)*(x - theta)
+  
+  eq8 <- 2*(1 - s)*(psi - eta)*g.std - 2*s*(psi - eta)*g.std
+  
+  eq <- c(eq1, eq2, eq3, eq4, eq5, eq6, eq7, eq8) 
+  return(eq)
+  
+}
+
 # Kernel weighted least squares
-kern_ipw <- function(a.new, a, psi, bw = 1, se.fit = FALSE) {
+kern_ipw <- function(a.new, a, s, x, psi, astar, astar2, cmat, ipw, bw = 1, se.fit = FALSE) {
+  
+  n <- length(a)
+  m <- ncol(x)
+  
+  # Gaussian Kernel
+  a.std <- (a - a.new) / bw
+  k.std <- dnorm(a.std) / bw
+  g.std <- cbind((1 - s)*a.std, s*a.std, (1 - s), s)
+  
+  b <- lm(psi ~ -1 + g.std, weights = k.std)$coefficients
+  mu <- unname(b[3] - b[4])
+  
+  if (se.fit) {
+    
+    U <- matrix(0, ncol = 5*m + 2, nrow = 5*m + 2)
+    V <- matrix(0, ncol = 5*m + 6, nrow = 4)
+    meat <- matrix(0, ncol = 5*m + 6, nrow = 5*m + 6)
+    theta <- colMeans(x[s == 0, ])
+    eta <- c(g.std%*%b)
+    
+    for (i in 1:n) {
+      
+      U[1:(4*m + 2),1:(4*m + 2)] <- U[1:(4*m + 2),1:(4*m + 2)] - ipw[i] * tcrossprod(cmat[i,])
+      U[(2*m + 3):(3*m + 2),(4*m + 3):(5*m + 2)] <- U[(2*m + 3):(3*m + 2),(4*m + 3):(5*m + 2)] - diag(1 - s[i], m, m)
+      U[(3*m + 3):(4*m + 2),(4*m + 3):(5*m + 2)] <- U[(3*m + 3):(4*m + 2),(4*m + 3):(5*m + 2)] - diag(s[i], m, m)
+      U[(4*m + 3):(5*m + 2),(4*m + 3):(5*m + 2)] <- U[(4*m + 3):(5*m + 2),(4*m + 3):(5*m + 2)] - diag(1 - s[i], m, m)
+      
+      V[,1:(4*m + 2)] <- V[,1:(4*m + 2)] + 2*(2*s[i] - 1)*k.std[i]*(psi[i] - eta[i])*tcrossprod(g.std[i,],cmat[i,])
+      V[,(5*m + 3):(5*m + 6)] <- V[,(5*m + 3):(5*m + 6)] + 2*(2*s[i] - 1)*k.std[i]*tcrossprod(g.std[i,])
+      
+      meat <- meat + tcrossprod(esteq(p = ipw[i], s = s[i], x = x[i,], psi = psi[i], g.std = g.std[i,],
+                                      astar = astar[i], astar2 = astar2[i], eta = eta[i], theta = theta))
+      
+    }
+    
+    invbread <- matrix(0, nrow = 5*m + 6, ncol = 5*m + 6)
+    invbread[1:(5*m + 2),1:(5*m + 2)] <- U
+    invbread[(5*m + 3):(5*m + 6), ] <- V
+    
+    bread <- try(solve(invbread), silent = TRUE)
+    
+    if (inherits(bread, "try-error")) {
+      
+      sandwich <- NA
+      variance <- NA
+      
+    } else {
+      
+      sandwich <- bread %*% meat %*% t(bread)
+      variance <- sandwich[5*m + 5, 5*m + 5] + sandwich[5*m + 6, 5*m + 6] 
+      
+    }
+    
+    return(c(mu = mu, sig2 = variance))
+    
+    
+  } else
+    return(mu)
+  
+}
+
+# Kernel weighted least squares
+kern_naive <- function(a.new, a, psi, bw = 1, se.fit = FALSE) {
   
   n <- length(a)
   
@@ -131,23 +182,6 @@ kern_ipw <- function(a.new, a, psi, bw = 1, se.fit = FALSE) {
     
     
   } else
-    return(mu)
-  
-}
-
-# Spline -- IPW
-spl_ipw <- function(a, psi, a.vals, df = 4, se.fit = FALSE) {
-  
-  n <- length(a)
-  
-  # spline model
-  fmla <- as.formula(paste0("psi ~ -1 + ns(a, df =", df, ", intercept = TRUE)"))
-  mod <- lm(fmla, data.frame(psi = psi, a = a))
-  mu <- predict(mod, newdata = data.frame(a = a.vals), se.fit = se.fit, type = "response")
-  
-  if (se.fit) 
-    return(rbind(mu = mu$fit, sig2 = mu$se.fit^2))
-  else
     return(mu)
   
 }
